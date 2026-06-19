0ff4d1ae874fa9ceab96f9a3c2b078be59ad024e
isemperador <ismaeljhostinbracoruiz@gmail.com>
2026-06-19 12:43:39 -0500

    Variables de Railway añadidas al config

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
-$host = 'localhost';
-$db   = 'lectura_veloz1';     // ← Actualizado
+$host = 'mysql.railway.internal';
+$db   = 'railway';
 $user = 'root';
-$pass = '';
+$pass = 'lcpVbBNVqzqrBhgVNiSbuLjhuRygrecG';
+$port = '3306'; // Suele ser 3306 o similar
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    // Añadimos el port a la cadena de conexión de PDO
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("❌ Error de conexión: " . $e->getMessage());
